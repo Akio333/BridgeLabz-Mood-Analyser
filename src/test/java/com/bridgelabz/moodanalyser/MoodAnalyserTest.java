616146d1be262ef93462c6a961d75348f9a7ad95
Akio333 <smulye10@gmail.com>
2021-01-18 11:42:40 +0530

    ADDED: Test for sad mood

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -15,10 +15,11 @@ public class MoodAnalyserTest
      * Rigorous Test :-)
      */
     @Test
-    public void testMoodAnalysis() throws Exception {
+    public void shouldReturn_SAD_Mood() throws Exception {
         MoodAnalyser moodAnalyser=new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("This is a sad message.");
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
+        Assert.assertEquals("SAD",mood);
     }
 }
 
